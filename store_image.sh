898453e1e1decf8b388b62eed9b842677762b60d
Samuel Oluwade <samueloluwade.devops@gmail.com>
2024-06-27 10:45:42 -0500

    updated deployment manifest and store image script

--- store_image.sh
+++ store_image.sh
@@ -5,3 +5,4 @@ aws ecr create-repository --repository-name moon-facts-service
 docker build -t moon-facts-service .
 docker tag moon-facts-service:latest 533266979424.dkr.ecr.us-east-1.amazonaws.com/moon-facts-service:latest
 docker push 533266979424.dkr.ecr.us-east-1.amazonaws.com/moon-facts-service:latest
+docker rmi 533266979424.dkr.ecr.us-east-1.amazonaws.com/moon-facts-service:latest
